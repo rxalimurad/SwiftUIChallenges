9345a45ed21629526ed074dec7656c2d3197a82e
alimurad <amurad@i2cinc.com>
2023-01-09 14:43:40 +0500

    Initial Commit

---
+++ A/AApp.swift
@@ -0,0 +1,17 @@
+//
+//  AApp.swift
+//  A
+//
+//  Created by Ali Murad on 09/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
